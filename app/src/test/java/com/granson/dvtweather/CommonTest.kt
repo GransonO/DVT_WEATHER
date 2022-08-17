9bdcf93ca60bd9cb90fb3cd03c4031caca125add
GransonO <oyombegranson@gmail.com>
2022-08-17 14:18:18 +0300

    Added Common Utils Test

---
+++ app/src/test/java/com/granson/dvtweather/CommonTest.kt
@@ -0,0 +1,109 @@
+package com.granson
+
+import com.granson.dvtweather.enums.WeatherEnums
+import com.granson.dvtweather.utils.Common.selectedWeatherEnums
+import org.junit.Test
+
+internal class CommonTest {
+
+    @Test
+    fun getSelectedWeatherEnums() {
+        assert(selectedWeatherEnums.value == WeatherEnums.SUNNY)
+    }
+
+    @Test
+    fun setSelectedWeatherEnums() {
+    }
+
+    @Test
+    fun getMainWeatherEnums() {
+    }
+
+    @Test
+    fun setMainWeatherEnums() {
+    }
+
+    @Test
+    fun getUserCurrentLocation() {
+    }
+
+    @Test
+    fun setUserCurrentLocation() {
+    }
+
+    @Test
+    fun getUserLocationDetails() {
+    }
+
+    @Test
+    fun setUserLocationDetails() {
+    }
+
+    @Test
+    fun getCurrentTemp() {
+    }
+
+    @Test
+    fun getDailyWeather() {
+    }
+
+    @Test
+    fun setDailyWeather() {
+    }
+
+    @Test
+    fun getDaysOrder() {
+    }
+
+    @Test
+    fun setDaysOrder() {
+    }
+
+    @Test
+    fun getSelectedPlaceWeatherEnums() {
+    }
+
+    @Test
+    fun setSelectedPlaceWeatherEnums() {
+    }
+
+    @Test
+    fun getSTORM_IDS() {
+    }
+
+    @Test
+    fun getCLOUDY_IDS() {
+    }
+
+    @Test
+    fun getDRIZZLE_IDS() {
+    }
+
+    @Test
+    fun getRAIN_IDS() {
+    }
+
+    @Test
+    fun getCLEAR_IDS() {
+    }
+
+    @Test
+    fun getSNOW_IDS() {
+    }
+
+    @Test
+    fun baseLogger() {
+    }
+
+    @Test
+    fun getWeatherEnum() {
+    }
+
+    @Test
+    fun daysFlow() {
+    }
+
+    @Test
+    fun getGetCurrentDate() {
+    }
+}
